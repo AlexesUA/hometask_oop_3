a2125d56cee524fbd694fb4fa41c7fd91e7906e6
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-03-28 17:01:12 +0300

    Fill Car class

--- src/task3/Саг.java
+++ src/task3/Саг.java
@@ -1,4 +1,21 @@
 package task3;
 
 public class Саг extends Vehicle {
+    private char classCar;
+
+    public Саг(double price, int yearManufacture, double speed) {
+        super(price, yearManufacture, speed);
+    }
+    public Саг(double price, int yearManufacture, double speed, char classCar) {
+        this(price, yearManufacture, speed);
+        this.classCar = classCar;
+    }
+
+    public char getClassCar() {
+        return classCar;
+    }
+
+    public void setClassCar(char classCar) {
+        this.classCar = classCar;
+    }
 }
