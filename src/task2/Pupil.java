ae0ffd3cab3101315d9d8e61a9413a29c77419c9
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-03-28 13:01:58 +0300

    Create Pupil class

---
+++ src/task2/Pupil.java
@@ -0,0 +1,17 @@
+package task2;
+
+public class Pupil {
+
+    public void study(){
+        System.out.println("Всі Добре навчаються");
+    }
+    public void read(){
+        System.out.println("Всі гарно читають");
+    }
+    public void write(){
+        System.out.println("Всі гарно пишуть");
+    }
+    public void relax(){
+        System.out.println("Всі гарно відпочивають");
+    }
+}
