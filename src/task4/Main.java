b7c7ca5941a22c04d762284be9f1d1aa6ca7045f
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-03-28 22:28:49 +0300

    finish task

--- src/task4/Main.java
+++ src/task4/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scInput = new Scanner(System.in);
         System.out.print("Введіть ліцензійний ключ: ");
-        String licenseKey = scInput.next();
+        String licenseKey = scInput.nextLine();
 
         // Це я погодився з Idea замінити звичайний switch на розширений. Цікавий метод запису.
         DocumentWorker document = switch (licenseKey) {
